0024723786a5dac82031fd2fb86709caa5f0f24d
Michael Beck <brit228@gmail.com>
2019-09-05 15:12:38 -0400

    Changed cors

--- app/__init__.py
+++ app/__init__.py
@@ -10,15 +10,21 @@ from .schemas import predictSchema, db
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_url_rule('/graph', view_func=GraphQLView.as_view('graphql', schema=predictSchema, graphiql=True))
 
-@app.after_request
-def after_request(response):
-    header = response.headers
-    header['Access-Control-Allow-Origin'] = '*'
-    header['Access-Control-Allow-Headers'] = '*'
-    return response
+@app.route('/', methods=['GET'])
+def checkInBase():
+    uid = request.get_json().get('uid', '')
+    if uid != '':
+        try:
+            auth.get_user(uid)
+        except ValueError:
+            return '{"error": "invalid uid"}'
+        except auth.AuthError:
+            return '{"error": "user does not exist"}'
+        docs = db.collection('users').where('uid', '==', uid).stream()
+        doc = [d for d in docs][0]
+        return '{"limit": '+doc.get('limit')+'}'
 
 @app.route('/complete', methods=['POST'])
 def completeRegistration():
@@ -26,7 +32,7 @@ def completeRegistration():
     typ = request.get_json().get('typ', '')
     if uid != '' and typ != '':
         try:
-            print(auth.get_user(uid))
+            auth.get_user(uid)
         except ValueError:
             return "ERROR: invalid uid"
         except auth.AuthError:
