417c91a0f83aeaa2dc39fabdba944c7cd5acbf71
Michael Beck <brit228@gmail.com>
2019-09-05 14:19:05 -0400

    Added new searches for GraphQL

--- app/schemas.py
+++ app/schemas.py
@@ -22,6 +22,14 @@ db = firestore.client()
 publisher = pubsub.PublisherClient()
 topic_path = publisher.topic_path('biopred', 'jobs')
 
+class PredictionResult(graphene.ObjectType):
+    sequence1 = graphene.String()
+    sequence2 = graphene.String()
+    predSubSeqItem1 = graphene.Boolean()
+    predSubSeqItem2 = graphene.Boolean()
+    predSubSeqItem3 = graphene.Boolean()
+    result = graphene.List(graphene.List(graphene.Float))
+    timestamp = graphene.Timestamp()
 
 class Item(graphene.InputObjectType):
     sequence = graphene.String()
@@ -39,6 +47,8 @@ class PredictionList(graphene.InputObjectType):
 
 class Query(graphene.ObjectType):
     post_prediction_jobs = graphene.Field(graphene.List(graphene.String), inputs=graphene.Argument(PredictionList, required=True))
+    get_prediction_results = graphene.Field(graphene.List(PredictionResult), inputs=graphene.Argument(graphene.Int))
+    get_my_prediction_results = graphene.Field(graphene.List(PredictionResult), inputs=graphene.Argument(graphene.Int))
 
     def resolve_post_prediction_jobs(parent, info, inputs):
         uid = info.context.get_json().get('authentication', {}).get('uid', '')
@@ -61,5 +71,55 @@ class Query(graphene.ObjectType):
             publisher.publish(topic_path, doc_ref.path.encode('utf-8'))
             output.append(doc_ref.path)
         return output
+
+    def resolve_get_prediction_results(parent, info, inputs):
+        docs = db.collection('jobs').where('status', '==', 'complete').order_by(
+            'timestamp', direction=firestore.Query.DESCENDING
+        ).limit(1000).stream()
+        i = 0
+        out = []
+        for d in docs:
+            if i < inputs*20 and i > (inputs-1)*20:
+                out.append({
+                    "sequence1": d.get('item1').get('sequence'),
+                    "sequence2": d.get('item2').get('sequence'),
+                    "predSubSeqItem1": d.get('predSubSeqItem1'),
+                    "predSubSeqItem2": d.get('predSubSeqItem2'),
+                    "predSubSeqItem3": d.get('item2').get('searchType') != 'ALL',
+                    "results": d.get('results'),
+                    "timestamp": d.get('timestamp')
+                })
+            elif i >= inputs*20:
+                break
+            i += 1
+        return out
+
+    def resolve_get_my_prediction_results(parent, info, inputs):
+        uid = info.context.get_json().get('authentication', {}).get('uid', '')
+        atk = info.context.get_json().get('authentication', {}).get('accessToken', '')
+        assert uid != '' and atk != ""
+        assert uid == auth.verify_id_token(atk)['uid']
+        docs = [d for d in db.collection('users').where('uid', '==', uid).stream()]
+        assert len(docs) > 0
+        docs = db.collection('jobs').where('uid', '==', uid).where('status', '==', 'complete').order_by(
+            'timestamp', direction=firestore.Query.DESCENDING
+        ).limit(1000).stream()
+        i = 0
+        out = []
+        for d in docs:
+            if i < inputs*20 and i > (inputs-1)*20:
+                out.append({
+                    "sequence1": d.get('item1').get('sequence'),
+                    "sequence2": d.get('item2').get('sequence'),
+                    "predSubSeqItem1": d.get('predSubSeqItem1'),
+                    "predSubSeqItem2": d.get('predSubSeqItem2'),
+                    "predSubSeqItem3": d.get('item2').get('searchType') != 'ALL',
+                    "results": d.get('results'),
+                    "timestamp": d.get('timestamp')
+                })
+            elif i >= inputs*20:
+                break
+            i += 1
+        return out
 
 predictSchema = graphene.Schema(query=Query, types=[Item, Prediction, PredictionList])
